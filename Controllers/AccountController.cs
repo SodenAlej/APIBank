6b2826a4ee13f96019cb2531cf46583e819068b1
SodenAlej <sodenisland@hotmail.com>
2024-04-28 19:59:37 -0600

    Etapa 5 - Agregar Policy "Viewer" a Controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace BankAPI.Controllers;
 
+[Authorize(Policy = "Viewer")]
 [ApiController]
 [Route("api/[controller]")]
 public class AccountController : ControllerBase
